7d20cc79dbfad0bf42ad4925c5fcb71841f19f5c
Szymon Kozioł <szymonkoziol3@gmail.com>
2025-05-18 23:24:32 +0200

    Create collection

--- backend/app/modules/core/crud_views.py
+++ backend/app/modules/core/crud_views.py
@@ -2,13 +2,25 @@ from fastapi import APIRouter
 from sqlmodel import select
 
 from app.db import DbSession
-from app.models import Collection, CollectionPublic
+from app.models import Collection, CollectionCreate, CollectionPublic
 
-collection_routes = APIRouter()
+router = APIRouter()
 
 
-@collection_routes.get("/", response_model=list[CollectionPublic])
+@router.get("/collections/", response_model=list[CollectionPublic], tags=["links"])
 async def read_collections(session: DbSession) -> list[CollectionPublic]:
     """Get all collections."""
     collections = await session.exec(select(Collection))
     return [CollectionPublic.model_validate(collection) for collection in collections]
+
+
+@router.post("/collections/", response_model=CollectionPublic, tags=["links"])
+async def create_collection(
+    body: CollectionCreate, session: DbSession
+) -> CollectionPublic:
+    """Create a new collection."""
+    collection = Collection.model_validate(body)
+    session.add(collection)
+    await session.commit()
+    await session.refresh(collection)
+    return CollectionPublic.model_validate(collection)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 
-from app.modules.core.crud_views import collection_routes
+from app.modules.core.crud_views import router as crud_routes
 
 api_router = APIRouter()
 
-api_router.include_router(
-    collection_routes, prefix="/collections", tags=["collections"]
-)
+api_router.include_router(crud_routes)
