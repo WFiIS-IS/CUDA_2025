5bea98e0e8a0e2ac122afe29724733b8acfa3e4b
Szymon Kozioł <szymonkoziol3@gmail.com>
2025-05-18 17:57:26 +0200

    Proxy configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,6 @@ from fastapi import FastAPI
 from app.config import settings
 from app.routes import api_router
 
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-)
+app = FastAPI(title=settings.PROJECT_NAME, root_path="/api")
 
 app.include_router(api_router)
