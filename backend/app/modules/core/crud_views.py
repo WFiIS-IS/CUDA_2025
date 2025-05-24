fceffeedc521e1da6525398a55e88cbbbcb52212
Szymon Kozioł <szymonkoziol3@gmail.com>
2025-05-24 11:45:03 +0200

    Added more crud routes

--- backend/app/modules/core/crud_views.py
+++ backend/app/modules/core/crud_views.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+import uuid
+from http import HTTPStatus
+
+from fastapi import APIRouter, HTTPException, Response
 from sqlmodel import select
 
 from app.db import DbSession
@@ -24,3 +27,19 @@ async def create_collection(
     await session.commit()
     await session.refresh(collection)
     return CollectionPublic.model_validate(collection)
+
+
+@router.delete(
+    "/collections/{collection_id}", response_model=CollectionPublic, tags=["links"]
+)
+async def delete_collection(collection_id: uuid.UUID, session: DbSession):
+    """Delete a collection."""
+    collection = await session.get(Collection, collection_id)
+    if not collection:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f'Collection with id "{collection_id}" not found',
+        )
+    await session.delete(collection)
+    await session.commit()
+    return Response(status_code=HTTPStatus.NO_CONTENT)
